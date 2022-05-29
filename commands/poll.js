badd0191eaca16bd1434cecac8f3d34e951c021f
Himanshu Khaitan <himanshukhaitan108@gmail.com>
2022-05-29 17:41:51 +0530

    /poll configured

--- commands/poll.js
+++ commands/poll.js
@@ -3,5 +3,35 @@ const { poll } = require("../utils/commands");
 
 const data = new SlashCommandBuilder()
   .setName(poll)
-  .setDescription("Create a poll");
+  .setDescription("Create a poll")
+  .addStringOption((option) =>
+    option
+      .setName("question")
+      .setDescription("The question of the poll")
+      .setRequired(true)
+  )
+  .addStringOption((option) =>
+    option
+      .setName("option-one")
+      .setDescription("The first option")
+      .setRequired(true)
+  )
+  .addStringOption((option) =>
+    option
+      .setName("emoji-one")
+      .setDescription("The emoji of the first option")
+      .setRequired(true)
+  )
+  .addStringOption((option) =>
+    option
+      .setName("option-two")
+      .setDescription("The second option")
+      .setRequired(true)
+  )
+  .addStringOption((option) =>
+    option
+      .setName("emoji-two")
+      .setDescription("The emoji of the second option")
+      .setRequired(true)
+  );
 module.exports = data;
--- components/poll.js
+++ components/poll.js
@@ -1,5 +1,35 @@
 const { MessageEmbed } = require("discord.js");
 
-const Poll = async (interaction) => {};
+const { embedColor, authorInfo } = require("../config");
+
+const Poll = async (interaction) => {
+  const question = interaction.options.getString("question");
+  const optionOne = interaction.options.getString("option-one");
+  const emojiOne = interaction.options.getString("emoji-one");
+  const optionTwo = interaction.options.getString("option-two");
+  const emojiTwo = interaction.options.getString("emoji-two");
+
+  const embed = new MessageEmbed()
+    .setColor(embedColor)
+    .setAuthor({ ...authorInfo, name: "Poll Alert 🥳" })
+    .setDescription(
+      `**${question}**\n\n${emojiOne} **-** ${optionOne}\n${emojiTwo} **-** ${optionTwo}`
+    )
+    .setFooter({
+      text: "Poll created by " + interaction.user.username,
+      iconURL: interaction.user.avatarURL(),
+    })
+    .setTimestamp();
+  await interaction.reply({ content: "Poll Created", ephemeral: true });
+
+  await interaction.channel
+    .send({
+      embeds: [embed],
+    })
+    .then(async (embedMessage) => {
+      embedMessage.react(emojiOne);
+      embedMessage.react(emojiTwo);
+    });
+};
 
 module.exports = { Poll };
