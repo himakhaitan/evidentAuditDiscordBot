7a4331bec3477fdd31d475fbf21a761e790079dd
Himanshu Khaitan <himanshukhaitan108@gmail.com>
2022-06-02 22:50:10 +0530

    bump_remind completed 🌳

--- components/bump_remind.js
+++ components/bump_remind.js
@@ -1,8 +1,28 @@
-const BumpRemind = async () => {
-    
-}
+const { MessageEmbed } = require("discord.js");
+
+// Config Import
+const { authorInfo, embedColor } = require("../config");
+
+const BumpRemind = async (interaction) => {
+  const embed = new MessageEmbed()
+    .setColor(embedColor)
+    .setAuthor({
+      ...authorInfo,
+      name: "Bump Reminder",
+    })
+    .setDescription(`Please run ${"```/bump```"}\n\nThanks for the reminder <@${interaction.user.id}>!`)
+    .setFooter({
+      text: `Reminded by ${interaction.user.username}#${interaction.user.discriminator}`,
+      iconURL: interaction.user.displayAvatarURL(),
+    })
+    .setTimestamp();
+
+  await interaction.reply({
+    content: `‼️[BUMP REMINDER]‼️ <@&${process.env.MODS}>`,
+    embeds: [embed],
+  });
+};
 
 module.exports = {
-    BumpRemind,
-
-}
+  BumpRemind,
+};
